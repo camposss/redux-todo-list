b9c8eae5cd950535fd2f4f72b6bec624ca524eb4
camposss <ccamposs23@gmail.com>
2018-01-14 07:53:56 -0800

    working on the delte button

--- src/components/list_item.js
+++ src/components/list_item.js
@@ -4,6 +4,11 @@ import {Link} from 'react-router-dom';
 export default props =>{
     console.log('list item props', props);
     return (
-        <li className='list-group-item'><Link to={`/item/${props._id}`}>{props.title}</Link></li>
+        <li className='list-group-item'>
+            <Link to={`/item/${props._id}`}>{props.title}</Link>
+            <button type="button" className='close' onClick={()=>console.log('clicked the span button and its index', props.key)}>
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </li>
     )
 }
